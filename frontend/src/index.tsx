c2675b6d875c3a05eca78a9147011f49fba3c3fc
Denis Matveev <strigon33@gmail.com>
2020-10-29 02:18:40 +1300

    Added search field with delayed search

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,35 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import { createStore, compose, Action } from "redux";
+import { createStore, compose, Action, Dispatch } from "redux";
 import { Provider } from "react-redux";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react'
 
 enum ActionType {
+	SearchCompleted
+}
 
+function buildApiUrl(path: string): URL {
+	return new URL(`http://localhost/api/${path}`);
+}
+
+interface SearchResults {
+
+}
+
+function search(query: string): Promise<SearchResults> {
+	const url = buildApiUrl(`search`);
+	const queryParameters: Record<string, string> = {
+		query
+	};
+	url.search = new URLSearchParams(queryParameters).toString();
+
+	return fetch(url.toString())
+		.then(response => response.json());
 }
 
 interface IState {
@@ -39,15 +59,36 @@ const store = createStore(
 	} as IState as any
 )
 
-const persistor = persistStore(store);
+function ImageDisplay(): JSX.Element {
+	return null;
+}
 
+function ImageRack(): JSX.Element {
+	return null;
+}
+
+function ImageSearch(): JSX.Element {
+	const [searchTaskTimeout, setSearchTaskTimeout] = React.useState<NodeJS.Timeout | null>(null);
+	return <Container>
+		<Form.Control type="text" placeholder="Search" onChange={(event) => {
+			if (searchTaskTimeout) {
+				clearTimeout(searchTaskTimeout);
+			}
+			const value = event.target.value;
+			setSearchTaskTimeout(setTimeout(() => {
+				setSearchTaskTimeout(null);
+				search(value).then(console.log);
+			}, 3000));
+		}} />
+	</Container>
+}
+
+const persistor = persistStore(store);
 ReactDOM.render(
 	<Provider store={store}>
 		<PersistGate loading={null} persistor={persistor}>
 			<BrowserRouter>
-				<Container>
-					Hello World
-				</Container>
+				<ImageSearch/>
 			</BrowserRouter>
 		</PersistGate>
 	</Provider>,
