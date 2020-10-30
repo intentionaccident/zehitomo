c9a3510da433e07c3a7a55972939086a2d6b7dc9
Denis Matveev <strigon33@gmail.com>
2020-10-30 17:33:32 +1300

    Add support for empty groups

--- frontend/src/components/FavouriteGroupsRack.tsx
+++ frontend/src/components/FavouriteGroupsRack.tsx
@@ -1,12 +1,23 @@
 import * as React from "react";
 import Container from "react-bootstrap/esm/Container";
 import { useSelector } from "react-redux";
-import { State } from "../State";
+import { ImageGroup, State } from "../State";
 import { splitIntoGroups } from "./ImageRack";
 import styles from "../styles.sass";
 import { BasicImageDisplay } from "./BasicImageDisplay";
 import { Link } from "react-router-dom";
 import { DeleteFavouritesGroupButton } from "./DeleteFavouritesGroupButton";
+
+
+function GroupActionPanel(props: {group: ImageGroup}): JSX.Element {
+	return <div className={`d-flex align-items-center p-2 ${styles.favouriteGroupActionBar}`}>
+		<Link className="text-white" to={`/favourites/${props.group.id}`}>
+			{props.group.name}
+		</Link>
+		<div className="flex-fill"/>
+		<DeleteFavouritesGroupButton group={props.group} />
+	</div>
+}
 
 export function FavouriteGroupRack(): JSX.Element {
 	const groups = useSelector((state: State) => state.imageGroups);
@@ -26,18 +37,15 @@ export function FavouriteGroupRack(): JSX.Element {
 				<div className={styles.imageColumn} key={column.index}>
 					{column.things.map(preview =>
 						<div key={preview.group.id} className={`${column.index === 0 ? "" : "ml-3"} mb-3`}>
-							<BasicImageDisplay image={preview.preview}>
-								<div className={`${styles.favouriteGroupPreview} d-flex flex-column`}>
-									<div className="flex-fill"/>
-									<div className={`d-flex align-items-center p-2 ${styles.favouriteGroupActionBar}`}>
-										<Link className="text-white" to={`/favourites/${preview.group.id}`}>
-											{preview.group.name}
-										</Link>
+							{ preview.preview
+								? <BasicImageDisplay image={preview.preview}>
+									<div className={`${styles.favouriteGroupPreview} d-flex flex-column`}>
 										<div className="flex-fill"/>
-										<DeleteFavouritesGroupButton group={preview.group} />
+										<GroupActionPanel group={preview.group}/>
 									</div>
-								</div>
-							</BasicImageDisplay>
+								</BasicImageDisplay>
+								: <GroupActionPanel group={preview.group}/>
+							}
 						</div>
 					)}
 				</div>
